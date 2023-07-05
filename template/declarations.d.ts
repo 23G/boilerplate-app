9f5392ad004a30ac8faa6c1b3e6e737ea5b1835e
Tycho <tycho@23g.nl>
2023-07-05 13:02:53 +0200

    Remove declaration, conflicts with other typings

--- template/declarations.d.ts
+++ template/declarations.d.ts
@@ -1,5 +1,3 @@
-import { Environment } from '@/types/environment';
-
 declare module '*.svg' {
     import React from 'react';
     import { SvgProps } from 'react-native-svg';
@@ -11,11 +9,4 @@ declare module '*.png' {
     import { FastImageProps } from 'react-native-fast-image';
     const value: number | FastImageProps.Source;
     export default value;
-}
-
-declare module 'react-native-config' {
-    interface NativeConfig {
-        API_URL: string;
-        ENV: Environment;
-    }
 }
