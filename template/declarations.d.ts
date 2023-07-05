9f35830debfe418d0de21f18aa6884233b650e6a
Tycho <tycho@23g.nl>
2023-07-05 12:47:32 +0200

    Add type declaration for `react-native-config`

--- template/declarations.d.ts
+++ template/declarations.d.ts
@@ -1,3 +1,5 @@
+import { Environment } from '@/types/environment';
+
 declare module '*.svg' {
     import React from 'react';
     import { SvgProps } from 'react-native-svg';
@@ -9,4 +11,11 @@ declare module '*.png' {
     import { FastImageProps } from 'react-native-fast-image';
     const value: number | FastImageProps.Source;
     export default value;
+}
+
+declare module 'react-native-config' {
+    interface NativeConfig {
+        API_URL: string;
+        ENV: Environment;
+    }
 }
